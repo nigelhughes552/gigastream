1b66a7b560875393bdd33a6b344ce9f205d63a2e
nigelhughes552 <nigel@theoldpo.plus.com>
2019-11-01 17:00:40 +0000

    edit image

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,23 +4,22 @@
 		<v-content>
 			<v-container>
 				<v-row style="margin-top:70px;" justify="end">
-					<nuxt-link to="/"> <img height="117px" width="150px" src="/gigastreamLogo.png"/></nuxt-link>
+					<nuxt-link to="/"> <v-img height="117px" width="150px" class="mr-5" src="/gigastreamLogo.png"/></nuxt-link>
 				</v-row>
 				<nuxt />
 			</v-container>
 		</v-content>
-<gigastream-footer></gigastream-footer>
-		
+		<gigastream-footer></gigastream-footer>
 	</v-app>
 </template>
 
 <script>
 import Navigation from '~/components/Navigation';
-import GigastreamFooter from '~/components/GigastreamFooter'
+import GigastreamFooter from '~/components/GigastreamFooter';
 export default {
 	components: {
-    Navigation,
-    GigastreamFooter
+		Navigation,
+		GigastreamFooter,
 	},
 };
 </script>
